314702d36e3619ac63907523926ea355685a478c
Erick1996-code <ochieng4273@gmail.com>
2024-04-18 16:17:15 +0300

    InvoiceReports

--- Litigation-Project/src/main/java/com/LDLS/Litigation/Project/BillingModule/Repositories/InvoiceReportsRepository.java
+++ Litigation-Project/src/main/java/com/LDLS/Litigation/Project/BillingModule/Repositories/InvoiceReportsRepository.java
@@ -1,9 +1,10 @@
 package com.LDLS.Litigation.Project.BillingModule.Repositories;
 
-import com.LDLS.Litigation.Project.BillingModule.Entities.Invoice;
+import com.LDLS.Litigation.Project.BillingModule.Entities.InvoiceReports;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface InvoiceRepository extends JpaRepository<Invoice,Long> {
+public interface InvoiceReportsRepository extends JpaRepository<InvoiceReports,Long> {
+
 }
---
+++ Litigation-Project/src/main/java/com/LDLS/Litigation/Project/BillingModule/Services/InvoiceReportsService.java
@@ -0,0 +1,34 @@
+package com.LDLS.Litigation.Project.BillingModule.Services;
+
+import com.LDLS.Litigation.Project.BillingModule.Entities.InvoiceReports;
+import com.LDLS.Litigation.Project.BillingModule.Repositories.InvoiceReportsRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Slf4j
+public class InvoiceReportsService {
+
+    private final InvoiceReportsRepository invoiceReportsRepository;
+
+    @Autowired
+    public InvoiceReportsService(InvoiceReportsRepository invoiceReportsRepository) {
+        this.invoiceReportsRepository = invoiceReportsRepository;
+    }
+
+    public InvoiceReports generateInvoiceReports(InvoiceReports invoiceReports) {
+        return invoiceReportsRepository.save(invoiceReports);
+    }
+
+    public List<InvoiceReports> getAllInvoicesReports() { return invoiceReportsRepository.findAll();
+    }
+
+    public InvoiceReports updateInvoiceReports(InvoiceReports invoiceReports) { return invoiceReports;
+    }
+
+    public void deleteInvoiceReports(Long id) {
+    }
+}
