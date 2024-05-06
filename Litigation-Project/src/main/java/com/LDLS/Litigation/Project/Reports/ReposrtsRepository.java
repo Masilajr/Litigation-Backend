83bc03abb7078cb0e9fbeb5151279a21048564db
Masilajr <mutuajoshua3436@gmail.com>
2024-05-06 17:07:32 +0300

    Report Generation

--- Litigation-Project/src/main/java/com/LDLS/Litigation/Project/Reports/ReposrtsRepository.java
+++ Litigation-Project/src/main/java/com/LDLS/Litigation/Project/Reports/ReposrtsRepository.java
@@ -9,4 +9,6 @@ import java.util.List;
 public interface ReposrtsRepository extends JpaRepository<UserRegistration, Long> {
     @Query(value = "select * from user_registration",nativeQuery = true)
     List<Object[]> getAllUsers();
+//    @Query(value = "SELECT id, first_name, last_name, loan_amount, amount_remaining, client_code, phone_no1, status FROM client WHERE status = $P{status}", nativeQuery = true)
+//    List<Object[]>
 }
--- Litigation-Project/src/main/java/com/LDLS/Litigation/Project/UserRegistration/DTO/UserRegistrationDTO.java
+++ Litigation-Project/src/main/java/com/LDLS/Litigation/Project/UserRegistration/DTO/UserRegistrationDTO.java
@@ -1,8 +1,66 @@
+//package com.LDLS.Litigation.Project.UserRegistration.DTO;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//import javax.persistence.Column;
+//
+//@Data
+//@NoArgsConstructor
+//@AllArgsConstructor
+//public class UserRegistrationDTO {
+//    private Long id;
+//    private String firstName;
+//    private String middleName;
+//    private String lastName;
+//    private String userId;
+//    private String email;
+////    private String privilege;
+//    private Long phoneNumber;
+//    private String branch;
+//    private String nationalIdNumber;
+//    private String role;
+//    private String gender;
+//    private String username;
+//    private boolean firstLogin = true;
+//    private String temporaryPassword;
+//    private String accessPeriod;
+//    private String country;
+//    private String status;
+//    @Column(name = "Locked")
+//    private boolean isAccountLocked;
+//
+//
+//    public UserRegistrationDTO(Long id, String firstName, String middleName, String lastName, String userId, String email, Long phoneNumber, String branch, String nationalIdNumber, String role, String gender, String username, boolean firstLogin, String accessPeriod, String country, String status) {
+//        this.id = id;
+//        this.firstName = firstName;
+//        this.middleName = middleName;
+//        this.lastName = lastName;
+//        this.userId = userId;
+//        this.email = email;
+//        this.phoneNumber = phoneNumber;
+//        this.branch = branch;
+//        this.nationalIdNumber = nationalIdNumber;
+//        this.role = role;
+//        this.gender = gender;
+//        this.username = username;
+//        this.firstLogin = firstLogin;
+//        this.accessPeriod = accessPeriod;
+//        this.country = country;
+//        this.status = status;
+//    }
+//
+//
+//
+//}
 package com.LDLS.Litigation.Project.UserRegistration.DTO;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
 
 @Data
 @NoArgsConstructor
@@ -14,7 +72,7 @@ public class UserRegistrationDTO {
     private String lastName;
     private String userId;
     private String email;
-    private String privilege;
+    //    private String privilege;
     private Long phoneNumber;
     private String branch;
     private String nationalIdNumber;
@@ -25,7 +83,9 @@ public class UserRegistrationDTO {
     private String temporaryPassword;
     private String accessPeriod;
     private String country;
-    private String status;
+    private String status; // Add status field here
+    @Column(name = "Locked")
+    private boolean isAccountLocked;
 
 
     public UserRegistrationDTO(Long id, String firstName, String middleName, String lastName, String userId, String email, Long phoneNumber, String branch, String nationalIdNumber, String role, String gender, String username, boolean firstLogin, String accessPeriod, String country, String status) {
@@ -44,9 +104,15 @@ public class UserRegistrationDTO {
         this.firstLogin = firstLogin;
         this.accessPeriod = accessPeriod;
         this.country = country;
-        this.status = status;
+        this.status = status; // Initialize status field
     }
 
+    // Getters and setters for the status field
+    public String getStatus() {
+        return status;
+    }
 
-
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
